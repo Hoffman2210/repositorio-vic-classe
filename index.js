0c96f97303af4fb1113e71b2c76cf705ba33aebe
Hoffman2210 <victoria.hoffman@escola.pr.gov.br>
2026-07-06 12:40:44 -0300

    vic

---
+++ index.js
@@ -0,0 +1,14 @@
+ class Pessoa {
+    nome = "Luana"
+    idade = 19
+
+    apresentar () {
+        console.log('Olá1 Meu nome é Luana e eu tenho 19 anos. ')
+ } 
+}
+
+let pessoa1 = new Pessoa();
+let pessoa2 = new Pessoa();
+
+pessoa1.apresentar();
+pessoa2.apresentar();
